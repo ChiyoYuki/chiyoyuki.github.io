a3b1cdd1a50b26ae459a4cf72741927ca07b094e
千代有希= <1911345567s@gmail.com>
2025-10-30 01:34:22 +0800

    把时区偏移写成定常数
    投降了，不会写根据用户端调整时区偏移

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,6 +1,6 @@
 export function formatDateToYYYYMMDDhhmmss(date: Date): string {
 	const nowDate = new Date();
-	const diffMinutes = nowDate.getTimezoneOffset();
-	const fixedDate = new Date(date.getTime() - diffMinutes * 60000);
+	const diffMinutes = 8 * 60;
+	const fixedDate = new Date(date.getTime() + diffMinutes * 60000);
 	return `${fixedDate.toISOString().substring(0, 10)} ${fixedDate.toISOString().substring(11, 16)}`;
 }
